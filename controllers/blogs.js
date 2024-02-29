f4211ba74886e317033982d3b0d1463ef884b2af
calumtreloar <calumtreloar@gmail.com>
2024-02-29 14:48:13 +1030

    Finished 4.13-4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,14 +7,36 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response, next) => {
-  const blog = new Blog(request.body);
+  const { title, author, url } = request.body;
 
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (exception) {
-    next(exception);
-  }
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: request.body.likes !== undefined ? request.body.likes : 0,
+  });
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const { title, author, url, likes } = request.body;
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  response.status(200).json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
@@ -8,92 +8,125 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-beforeEach(async () => {
-  await Blog.deleteMany({}, { maxTimeMS: 20000 });
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
-});
+describe("When there is initially some blogs saved", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany();
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+    const promiseArray = blogObjects.map(blog => blog.save());
+    await Promise.all(promiseArray);
+  });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("blogs identifier property is named id and not _id", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect(response => {
-      const blogs = response.body;
-      blogs.forEach(blog => {
-        console.log(blog);
-        if (!("id" in blog)) throw new Error("id property not found");
+  test("blogs identifier property is named id and not _id", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect(response => {
+        const blogs = response.body;
+        blogs.forEach(blog => {
+          console.log(blog);
+          if (!("id" in blog)) throw new Error("id property not found");
+        });
       });
+  });
+
+  describe("addition of a new blog", () => {
+    test("A valid blog can be added to the database", async () => {
+      const newBlog = {
+        _id: "5a422aa71b54a676234d17f2",
+        title: "17 Advanced SEO Techniques",
+        author: "Brian Dean",
+        url: "https://backlinko.com/advanced-seo",
+        likes: 1934,
+        __v: 0,
+      };
+
+      const response = await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      const addedBlog = response.body; // Use the response body instead of blogsAtEnd
+      const blogsAtEnd = await helper.blogsInDb();
+      const contents = blogsAtEnd.map(r => r.title);
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+      assert(contents.includes("17 Advanced SEO Techniques"));
     });
-});
 
-test("A valid blog can be added to the database", async () => {
-  const newBlog = {
-    _id: "5a422aa71b54a676234d17f2",
-    title: "17 Advanced SEO Techniques",
-    author: "Brian Dean",
-    url: "https://backlinko.com/advanced-seo",
-    likes: 1934,
-    __v: 0,
-  };
+    test("If blog is missing the likes property, it defaults to 0", async () => {
+      const newBlog = {
+        _id: "5a422aa71b54a676234d17f2",
+        title: "17 Advanced SEO Techniques",
+        author: "Brian Dean",
+        url: "https://backlinko.com/advanced-seo",
+        __v: 0,
+      };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const contents = blogsAtEnd.map(r => r.title);
+      const blogsAtEnd = await helper.blogsInDb();
+      const contents = blogsAtEnd.map(r => r.likes);
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-  assert(contents.includes("17 Advanced SEO Techniques"));
-});
+      assert(contents.includes(0));
+    });
 
-test("If blog is missing the likes property, it defaults to 0", async () => {
-  const newBlog = {
-    _id: "5a422aa71b54a676234d17f2",
-    title: "17 Advanced SEO Techniques",
-    author: "Brian Dean",
-    url: "https://backlinko.com/advanced-seo",
-    __v: 0,
-  };
+    test("If blog has no title or url properties, backend returns 400 bad request", async () => {
+      const newBlog = {
+        _id: "5a422aa71b54a676234d17f2",
+        author: "Brian Dean",
+        likes: 1934,
+        __v: 0,
+      };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+    });
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const contents = blogsAtEnd.map(r => r.likes);
+  describe("Deletion of a blog", () => {
+    test("succeeds with status code 204 if id is valid", async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToDelete = blogsAtStart[0];
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-  assert(contents.includes(0));
-});
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-test("If blog has no title or url properties, backend returns 400 bad request", async () => {
-  const newBlog = {
-    _id: "5a422aa71b54a676234d17f2",
-    author: "Brian Dean",
-    likes: 1934,
-    __v: 0,
-  };
+      const blogsAtEnd = await helper.blogsInDb();
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+      const contents = blogsAtEnd.map(r => r.title);
+      assert(!contents.includes(blogToDelete.title));
+    });
+  });
+
+  describe("Updating a blog", () => {
+    test("succeeds with status code 200 if id is valid", async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToUpdate = blogsAtStart[0];
+
+      const updatedLikes = blogToUpdate.likes + 100;
+      await api.put(`/api/blogs/${blogToUpdate.id}`).send({ likes: updatedLikes }).expect(200);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      const contents = blogsAtEnd.map(r => r.likes);
+      assert(contents.includes(updatedLikes));
+    });
+  });
 });
 
 after(async () => {
